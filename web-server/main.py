48a32ff6516eb4f3cb36e452278d4e67b2a6b141
root <root@DESKTOP-IV4KGDK.localdomain>
2022-11-25 05:45:22 -0500

    commit final

---
+++ web-server/main.py
@@ -0,0 +1,39 @@
+import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+@app.get("/")
+def get_list():
+    return [1, 2, 3, 4]
+
+
+@app.get("/contact")
+def get_contact():
+    return {
+        'name': 'platzi'
+    }
+
+
+@app.get("/items/", response_class=HTMLResponse)
+async def read_items():
+    return """
+    <html>
+        <head>
+            <title>Mauricio Combariza</title>
+        </head>
+        <body>
+            <h1>Full Stack and Computer Science!!</h1>
+        </body>
+    </html>
+    """
+
+
+def run():
+    store.get_categories()
+
+
+if __name__ == '__main__':
+    run()
